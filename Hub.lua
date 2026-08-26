a2ba6c6b23db523e0cfdae8fe0f66754990009ed
femmehomme90-web <femmehomme90@gmail.com>
2026-08-26 17:01:44 +0200

    hub: 21 game(s) / 22 script(s)

--- Hub.lua
+++ Hub.lua
@@ -1,7 +1,7 @@
 --[[
     VoidHub -- point d'entree unique
     Genere automatiquement par voidhub-bridge, ne pas editer a la main.
-    Build: 2026-08-26 09:43:23
+    Build: 2026-08-26 15:01:44
 ]]
 
 local UI_URL  = "https://raw.githubusercontent.com/csgo1compte-cloud/RayVoidUi/refs/heads/main/RayVoid"
@@ -23,6 +23,7 @@ local MAP = {
 	["10199301628"] = {{n="MEN",u="https://raw.githubusercontent.com/femmehomme90-web/RayVoid_Scripts/main/Merge-a-Nuke"}},
 	["10209534490"] = {{n="Chicken-Farm",u="https://raw.githubusercontent.com/femmehomme90-web/RayVoid_Scripts/main/Chicken-Farm"}},
 	["10261267004"] = {{n="Storage-Hunters-Open-World",u="https://raw.githubusercontent.com/femmehomme90-web/RayVoid_Scripts/main/Storage-Hunters-Open-World"}},
+	["10290463156"] = {{n="[] Blow up Lucky Blocks For Cars",u="https://raw.githubusercontent.com/femmehomme90-web/RayVoid_Scripts/main/77084002576266.lua"}},
 	["10336278580"] = {{n="Sell-Ores",u="https://raw.githubusercontent.com/femmehomme90-web/RayVoid_Scripts/main/Sell-Ores"}},
 	["10338952197"] = {{n="Grow-a-Chicken-Fighter",u="https://raw.githubusercontent.com/femmehomme90-web/RayVoid_Scripts/main/Grow-a-Chicken-Fighter"}},
 	["10406668651"] = {{n="Mine-a-Planet",u="https://raw.githubusercontent.com/femmehomme90-web/RayVoid_Scripts/main/Mine-a-Planet"}},
